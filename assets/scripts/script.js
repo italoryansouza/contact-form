2e49d421d7c26a9c9f780f9f98cb88029b0b7474
ÍtaloRyan <italoryansouza2019@gmail.com>
2024-06-10 23:52:45 -0300

    Montagem do HTML
    Além da montagem do HTML, adiantada uma boa parte do CSS, com um script básico de seleção

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+// Focus Inputs Radio
+
+function focusRadio1(){
+    document.getElementById("query1id").checked = true;
+}
+
+function focusRadio2(){
+    document.getElementById("query2id").checked = true;
+}
